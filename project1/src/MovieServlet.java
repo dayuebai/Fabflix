beb85cddd45a887e69ace8545703de016d275857
Dayue Bai <dayuebai@dhcp-v017-168.mobile.uci.edu>
2018-04-11 17:54:17 -0700

    Change mysql password

--- project1/src/MovieServlet.java
+++ project1/src/MovieServlet.java
@@ -25,7 +25,7 @@ public class MovieServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // change this to your own mysql username and password
 		String loginUser = "root";
-        String loginPasswd = "Mm960621@";
+        String loginPasswd = "mm941026";
         String loginUrl = "jdbc:mysql://localhost:3306/moviedb";
 		
         // set response mime type
